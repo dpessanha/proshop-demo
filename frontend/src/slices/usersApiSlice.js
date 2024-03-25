6e12e51562b486d3da1a9469fb34ba355f34b085
dpessanha <daopessanha@gmail.com>
2024-03-25 17:59:47 +0000

    Login Screen

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,10 +1,11 @@
-import { USERS_URL } from '../constants'
-import { apiSlice } from './apiSlice'
+import { USERS_URL } from '../constants.js'
+import { apiSlice } from './apiSlice.js'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
+        // eslint-disable-next-line no-undef
         url: USERS_URL / login,
         method: 'POST',
         body: data,
