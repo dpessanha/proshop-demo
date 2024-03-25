d1e83e1de7eafe168e6a6921e6ccb2c7be12dab4
dpessanha <daopessanha@gmail.com>
2024-03-25 19:54:26 +0000

    Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { Container } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
@@ -13,6 +15,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   )
 }
